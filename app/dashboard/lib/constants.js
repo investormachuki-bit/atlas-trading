4c92d79433b4003f9360eae96888c9b921963a1a
investormachuki-bit <investormachuki@gmail.com>
2026-08-27 10:50:06 +0300

    Create app/dashboard/lib/constants.js

---
+++ app/dashboard/lib/constants.js
@@ -0,0 +1,16 @@
+export const MARKET_ID =
+  "ee6cf0c3-1a03-48a9-9d76-7aa36ad62657";
+
+export const STRATEGY_ID =
+  "22aab602-2282-4393-bd9c-efa82171b6d5";
+
+export const BACKTEST_API =
+  "https://ookqbnpjtiqixamacalv.supabase.co/functions/v1/backtest-api";
+
+export const TIMEFRAME = "M5";
+
+export const RISK_PER_TRADE = 0.01;
+
+export const RISK_REWARD = 2;
+
+export const TOTAL_CANDLES = 329103;
