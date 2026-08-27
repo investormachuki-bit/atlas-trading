96fdfe93a6cf213db7193053ec4de22b3e0577e0
investormachuki-bit <investormachuki@gmail.com>
2026-08-27 11:27:52 +0300

    Update page.js

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 
-import { supabase } from "../lib/supabase";
+import { supabase } from "../../lib/supabase";
 
 import DashboardHeader from "./components/DashboardHeader";
 import MarketHeader from "./components/MarketHeader";
